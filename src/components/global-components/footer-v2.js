425f9bbb9a03055332736d251aa8dd16c57c574a
Mai Xuan Hien (FIS TSD HN) <hienmx@fpt.com.vn>
2021-07-31 04:31:46 +0700

    update

--- src/components/global-components/footer-v2.js
+++ src/components/global-components/footer-v2.js
@@ -24,49 +24,33 @@ class Footer_v2 extends Component {
           <div className="footer-top padding-bottom-50">
             <div className="container">
               <div className="row">
-            
-                <div className="col-lg-4 col-md-6 offset-lg-1">
-                  <div className="widget contact-widget">
-                    <h4 className="widget-title">GET IN TOUCH</h4>
-                    <ul className="contact_info_list">
-                      <li className="single-info-item">
-                        <div className="icon">
-                          <i className="icon-home-foother" />
-                        </div>
-                        <div className="details">
-                          <span>Tòa nhà Pico<br />173 Xuân Thủy, Dịch Vọng Hậu <br />Cầu Giây, Hà Nội</span>
-                        </div>
-                      </li>
-                      <li className="single-info-item">
-                        <div className="icon">
-                          <i className="icon-email-subscribe" />
-                        </div>
-                        <div className="details">
-                          linhhajc@gmail.com
-                        </div>
-                      </li>
-                      <li className="single-info-item">
-                        <div className="icon">
-                          <i className="icon-call-footer" />
-                        </div>
-                        <div className="details">
-                          <a href="tel:+496170961709">(+84) 0986-279-928, (+84) 0969-781-710</a>
-                        </div>
-                      </li>
-                    </ul>
+                <div className="col-lg-3 col-md-6 padding-bottom-30">
+                <div className="widget contact-widget">
+                    <h4 className="widget-title">SIGN UP TO RECEIVE 15% OFF</h4>
+                  </div>
+                  <div class="newsletter__wrap footer-newsletter__wrap input-placeholder js-field-group">
+                      <div class="form-inline__input input-placeholder">
+                          <label class="newsletter__label newsletter__label footer-newsletter__label" for="footerNewsletterInput">
+                              email
+                          </label>
+                          <input id="footerNewsletterInput" class="input newsletter__input footer-newsletter__input js-email" name="email" type="email" value="" required="" />
+                      </div>
+                      <button id="footerNewsletterSubmit" class="form-inline__btn newsletter__btn footer-newsletter__btn" type="submit">
+                          <span class="btn__label">
+                              <span class="icon icon--right-arrow"></span>
+                          </span>
+                      </button>
                   </div>
                 </div>
-                <div className="col-lg-2 col-md-6">
+                <div className="col-lg-1 col-md-6 offset-lg-1">
+                  
+                </div>
+                <div className="col-lg-4 col-md-6">
                   <div className="widget widget_nav_menu">
-                    <h4 className="widget-title">OUR POLICY</h4>
+                    <h4 className="widget-title">CONTACT</h4>
                    	 <ul className="stone-go-top">
-                          <li><Link to="/collection">Brands</Link></li>
-                          <li><Link to="/contact">Contact Us</Link></li>
-                          <li><Link to="/about">About</Link></li>
-                          <li><Link to="/blog">Blog</Link></li>
-                          <li><Link to="/blog-details">Blog details</Link></li>
-                          <li><Link to="/comming-soon">Comming Soon</Link></li>
-                          <li><Link to="/collection-full">Shop</Link></li>
+                          <li className="widget-title"><Link to="/collection"><i className="icon-home-foother" />173 XUÂN THỦY, CẦU GIẤY, HN</Link></li>
+                          <li className="widget-title"><Link to="/contact"><i className="icon-call-footer" />0986279928</Link></li>
                         </ul>
                   </div>
                 </div>
@@ -74,42 +58,15 @@ class Footer_v2 extends Component {
                   <div className="widget widget_nav_menu">
                     <h4 className="widget-title">INFORMATION</h4>
                     <ul className="stone-go-top">
-                          <li><Link to="/collection">Clothing</Link></li>
-                          <li><Link to="/collection-list">Accessories</Link></li>
-                          <li><Link to="/collection-full">Women</Link></li>
-                          <li><Link to="/collection-list">Men</Link></li>
-                          <li><Link to="/collection-full">Shoes</Link></li>
-                          <li><Link to="/collection">New Arrivals</Link></li>
-                          <li><Link to="/collection-list">Clearance</Link></li>
+                          <li><Link to="/collection">TIPS</Link></li>
+                          <li><Link to="/collection-list">FAQ</Link></li>
                      </ul>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="footer-bottom">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-6 align-self-center">
-                  <p>© Stoon 2020. Powered with <i className="fa fa-heart" /> by <a href="#">Codingeek</a>.</p>
-                </div>
-                <div className="col-md-6">
-                  <ul>
-                    <li><a href="#"><img src={publicUrl+"assets/img/payment/1.png"} alt="" /></a></li>
-                    <li><a href="#"><img src={publicUrl+"assets/img/payment/2.png"} alt="" /></a></li>
-                    <li><a href="#"><img src={publicUrl+"assets/img/payment/3.png"} alt="" /></a></li>
-                    <li><a href="#"><img src={publicUrl+"assets/img/payment/4.png"} alt="" /></a></li>
-                    <li><a href="#"><img src={publicUrl+"assets/img/payment/5.png"} alt="" /></a></li>
-                    <li><a href="#"><img src={publicUrl+"assets/img/payment/6.png"} alt="" /></a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </footer>
-
-
-
+          </footer>
         )
     }
 }
